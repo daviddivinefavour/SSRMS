342b06e8f949d0abef8be0ed77010a163724252a
Mercy Uzor <>
2024-07-13 10:54:01 +0100

    feat(course-reg): updated the design for course registration

--- src/app/portal/courses/register/components/CourseList.tsx
+++ src/app/portal/courses/register/components/CourseList.tsx
@@ -6,43 +6,44 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from '@/components/ui/table'
-import { Button } from '@/components/ui/button'
-import React from 'react'
+} from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+import React from "react";
+import { Checkbox } from "@/components/ui/checkbox";
 
 export type TCourseObj = {
-  id: number
-  title: string
-  code: string
-  unit: number
-}
+  id: number;
+  title: string;
+  code: string;
+  unit: number;
+};
 
 const courseList: TCourseObj[] = [
   {
     id: 1,
-    title: 'COMPUTER TECHNOLOGY I (OO BASIC)',
-    code: 'Com 211',
+    title: "COMPUTER TECHNOLOGY I (OO BASIC)",
+    code: "Com 211",
     unit: 4,
   },
   {
     id: 2,
-    title: 'INTRODUCTION TO SYSTEM PROGRAMMING',
-    code: 'Com 212',
+    title: "INTRODUCTION TO SYSTEM PROGRAMMING",
+    code: "Com 212",
     unit: 4,
   },
   {
     id: 3,
-    title: 'COMMERCIAL PROGRAMMING LANGUAGE',
-    code: 'Com 213',
+    title: "COMMERCIAL PROGRAMMING LANGUAGE",
+    code: "Com 213",
     unit: 4,
   },
   {
     id: 4,
-    title: 'FILE ORG. & MANAGEMENT',
-    code: 'Com 214',
+    title: "FILE ORG. & MANAGEMENT",
+    code: "Com 214",
     unit: 4,
   },
-]
+];
 
 export const CourseList = ({
   addToSelectedCourses,
@@ -52,61 +53,57 @@ export const CourseList = ({
     <Table>
       <TableHeader>
         <TableRow>
-          <TableHead>S/N</TableHead>
+          <TableHead></TableHead>
           <TableHead>Course Title</TableHead>
           <TableHead>Course Code</TableHead>
           <TableHead>Credit Unit</TableHead>
-          <TableHead className="text-right">Action</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
         {courseList.map((courseObj: TCourseObj) => {
+          const { id, ...courseDetails } = courseObj;
           return (
             <TableRow key={courseObj.code}>
-              {Object.values(courseObj).map((cellItem, index) => (
+              <TableCell className="text-right">
+                <Checkbox id={`course_${id}`} />
+              </TableCell>
+              {Object.values(courseDetails).map((cellItem, index) => (
                 <TableCell className="font-medium" key={`${cellItem}_${index}`}>
                   {cellItem}
                 </TableCell>
               ))}
-              <TableCell className="text-right">
-                <AddButton
-                  addToSelectedCourses={addToSelectedCourses}
-                  removeFromSelectedCourses={removeFromSelectedCourses}
-                  course={courseObj}
-                />
-              </TableCell>
             </TableRow>
-          )
+          );
         })}
       </TableBody>
     </Table>
-  )
-}
+  );
+};
 
 const AddButton = ({
   addToSelectedCourses,
   removeFromSelectedCourses,
   course,
 }) => {
-  const [registerCourse, setRegisterCourse] = React.useState(false)
+  const [registerCourse, setRegisterCourse] = React.useState(false);
   const handleButtonClick = () => {
     setRegisterCourse((prev) => {
       if (prev) {
-        removeFromSelectedCourses(course.id)
+        removeFromSelectedCourses(course.id);
       } else {
-        addToSelectedCourses(course)
+        addToSelectedCourses(course);
       }
 
-      return !prev
-    })
-  }
+      return !prev;
+    });
+  };
   return (
     <Button
-      variant={!registerCourse ? 'outline' : 'destructive'}
-      size={'sm'}
+      variant={!registerCourse ? "outline" : "destructive"}
+      size={"sm"}
       onClick={handleButtonClick}
     >
-      {!registerCourse ? 'Add' : 'Remove'}
+      {!registerCourse ? "Add" : "Remove"}
     </Button>
-  )
-}
+  );
+};
--- src/app/portal/courses/register/page.tsx
+++ src/app/portal/courses/register/page.tsx
@@ -1,5 +1,5 @@
-'use client'
-import { Card, CardContent, CardHeader } from '@/components/ui/card'
+"use client";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import {
   Select,
   SelectContent,
@@ -8,31 +8,41 @@ import {
   SelectLabel,
   SelectTrigger,
   SelectValue,
-} from '@/components/ui/select'
-import { CourseList, TCourseObj } from './components/CourseList'
-import { Label } from '@/components/ui/label'
-import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
-import SelectedCourseCard from './components/SelectedCourseCard'
-import { useState } from 'react'
-import StudentDataForm from './components/StudentDataForm'
+} from "@/components/ui/select";
+import { CourseList, TCourseObj } from "./components/CourseList";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import SelectedCourseCard from "./components/SelectedCourseCard";
+import { useState } from "react";
+import StudentDataForm from "./components/StudentDataForm";
+import { Button } from "@/components/ui/button";
+
+type TSelectSession = {
+  label: string;
+  value: string;
+};
+
+type TSelectionProps = {
+  sessions: TSelectSession[];
+};
 
 const sessions = [
   {
-    label: '2023/2024',
-    value: '2023/2024',
+    label: "2023/2024",
+    value: "2023/2024",
   },
   {
-    label: '2022/2023',
-    value: '2022/2023',
+    label: "2022/2023",
+    value: "2022/2023",
   },
-]
+];
 
 const levels = [
-  { key: 'nd-one', value: 'ND I' },
-  { key: 'nd-two', value: 'ND II' },
-  { key: 'hnd-one', value: 'HND I' },
-  { key: 'hnd-two', value: 'HND II' },
-]
+  { value: "nd-one", label: "ND I" },
+  { value: "nd-two", label: "ND II" },
+  { value: "hnd-one", label: "HND I" },
+  { value: "hnd-two", label: "HND II" },
+];
 
 const dummyCourses: TCourseObj[] = [
   // {
@@ -59,107 +69,160 @@ const dummyCourses: TCourseObj[] = [
   //   code: 'Com 214',
   //   unit: 4,
   // },
-]
+];
 
 const RegisterCourses = () => {
-  const [selectedCourses, setSelectedCourses] = useState<TCourseObj[]>([])
+  const [selectedCourses, setSelectedCourses] = useState<TCourseObj[]>([]);
+  const [courseDetails, setCourseDetails] = useState({
+    session: "",
+    level: "",
+    semester: "",
+  });
+  const [showCourseTable, setShowCourseTable] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const addToSelectedCourses = (course: TCourseObj) => {
-    setSelectedCourses([...selectedCourses, course])
-  }
+    setSelectedCourses([...selectedCourses, course]);
+  };
 
   const removeFromSelectedCourses = (id: number) => {
     setSelectedCourses((prevCourses) =>
       prevCourses.filter((course) => course.id !== id)
-    )
-  }
+    );
+  };
+
+  const handleFetchCourse = () => {
+    //TODO: while Check if the user has selected a course already in this collection and disable that course hence it's selected by that student already
+    setShowCourseTable(true);
+  };
 
   return (
     <>
-      <div className="grid grid-cols-1 gap-y-8">
-        <section>
-          <div className="flex justify-between mb-5">
-            <div className="">
-              <h3 className="font-semibold text-2md">Register Courses</h3>
-            </div>
-          </div>
-          <div className="grid grid-cols-1 gap-y-4">
-            <div className="flex gap-1 py-2 col-auto">
-              <Label htmlFor="level" className=" font-semibold">
-                Level:
-              </Label>
-              <RadioGroup
-                defaultValue={levels[0].key}
-                className="flex justify-between"
-              >
-                {levels.map((level) => {
-                  return (
-                    <div
-                      className="flex items-center space-x-2"
-                      key={level.key}
-                    >
-                      <RadioGroupItem value={level.key} id={level.key} />
-                      <Label htmlFor={level.key}>{level.value}</Label>
-                    </div>
-                  )
-                })}
-              </RadioGroup>
-            </div>
-            <div className="flex gap-1 py-2 col-auto">
-              <Label htmlFor="session" className=" font-semibold">
-                Sessions
-              </Label>
-              <SelectSession sessions={sessions} />
-            </div>
-            <div className="grid grid-cols-4 gap-x-3 col-auto">
-              <div className="col-span-3">
-                <Card>
-                  <CardHeader>
-                    <h4 className="mb-2 font-semibold">Available Courses</h4>
-                  </CardHeader>
-                  <CardContent className="px-2 pt-2">
-                    <CourseList
-                      addToSelectedCourses={addToSelectedCourses}
-                      removeFromSelectedCourses={removeFromSelectedCourses}
-                    />
-                  </CardContent>
-                </Card>
-              </div>
-              <div className="col-span-1">
-                <SelectedCourseCard courses={selectedCourses} maxCredit={30} />
-              </div>
-            </div>
-          </div>
-        </section>
-        <section>
-          <div className="flex justify-between mb-5">
-            <div className="">
-              <h3 className="font-semibold text-2md">Student Data</h3>
-            </div>
-          </div>
+      <div className="mb-5">
+        <h3 className="font-semibold text-2md">Register Courses</h3>
+      </div>
+      <div className="grid grid-cols-4 gap-x-3 col-auto">
+        <div className="col-span-1">
           <Card>
-            <CardHeader>
-              <h4 className="mb-2 font-semibold">Profile Information</h4>
-            </CardHeader>
-            <CardContent className="px-2 pt-2 mx-2">
-              <StudentDataForm />
+            <CardContent className="px-2 pt-2">
+              <div className="mt-4">
+                <Label className="mb-3 block font-semibold">
+                  Select Session
+                </Label>
+                <Select>
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Please select session" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {sessions.map((session) => (
+                      <SelectItem value={session.value} key={session.value}>
+                        {session.label}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </div>
+              <div className="mt-4">
+                <Label className="mb-3 block font-semibold">Select Level</Label>
+                <Select>
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Please select level" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {levels.map((level) => (
+                      <SelectItem key={level.value} value={level.value}>
+                        {level.label}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </div>
+              <div className="mt-4">
+                <Label className="mb-3 block font-semibold">Semester</Label>
+                <Select>
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Please Select a semester" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="first-semester">
+                      First Semester
+                    </SelectItem>
+                    <SelectItem value="second-semester">
+                      Second Semester
+                    </SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+              <div className="mt-10">
+                <Button className="w-full" onClick={handleFetchCourse}>
+                  Process
+                </Button>
+              </div>
             </CardContent>
           </Card>
-        </section>
+        </div>
+        <div className="col-span-3">
+          <div className="col-span-3">
+            {showCourseTable && (
+              <Card>
+                <CardHeader>
+                  <div className="flex justify-between">
+                    <h4 className="mb-2 font-semibold">Available Courses</h4>
+                    <p>Total Credit Unit: 0</p>
+                  </div>
+                </CardHeader>
+                <CardContent className="px-2 pt-2">
+                  <CourseList
+                    addToSelectedCourses={addToSelectedCourses}
+                    removeFromSelectedCourses={removeFromSelectedCourses}
+                  />
+                  <hr />
+                  <div className="flex justify-end mt-10">
+                    <Button className="" disabled={true}>
+                      Save Selected Course(s)
+                    </Button>
+                  </div>
+                </CardContent>
+              </Card>
+            )}
+          </div>
+        </div>
       </div>
+      {/* <div className="flex gap-1 py-2 col-auto">
+            <Label htmlFor="level" className=" font-semibold">
+              Level:
+            </Label>
+
+          </div>
+          <div className="flex gap-1 py-2 col-auto">
+            <Label htmlFor="session" className=" font-semibold">
+              Sessions
+            </Label>
+            <SelectSession sessions={sessions} />
+          </div> */}
+      {/* <div className="grid grid-cols-4 gap-x-3 col-auto">
+            <div className="col-span-3">
+              <Card>
+                <CardHeader>
+                  <h4 className="mb-2 font-semibold">Available Courses</h4>
+                </CardHeader>
+                <CardContent className="px-2 pt-2">
+                  <CourseList
+                    addToSelectedCourses={addToSelectedCourses}
+                    removeFromSelectedCourses={removeFromSelectedCourses}
+                  />
+                </CardContent>
+              </Card>
+            </div>
+            <div className="col-span-1">
+              <SelectedCourseCard courses={selectedCourses} maxCredit={30} />
+            </div>
+          </div> */}
     </>
-  )
-}
+  );
+};
 
-export default RegisterCourses
-type TSelectSession = {
-  label: string
-  value: string
-}
-
-type TSelectionProps = {
-  sessions: TSelectSession[]
-}
+export default RegisterCourses;
 
 const SelectSession = ({ sessions }: TSelectionProps) => {
   return (
@@ -177,5 +240,5 @@ const SelectSession = ({ sessions }: TSelectionProps) => {
         </SelectGroup>
       </SelectContent>
     </Select>
-  )
-}
+  );
+};
