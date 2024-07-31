f68db92b0103d9cafaa12fc5acaa12053af3b0cb
Divinefavour David <blazer0022@gmail.com>
2024-07-31 09:13:24 +0100

    initiate paystack popup for registering courses

--- src/app/portal/_components/molecules/Navbar.tsx
+++ src/app/portal/_components/molecules/Navbar.tsx
@@ -6,22 +6,9 @@ import { IoChevronDown } from 'react-icons/io5'
 export const dummyAvatarUrl =
   'https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes-thumbnail.png'
 
-const Navbar = () => {
+const Navbar = ({}) => {
   return (
     <nav className="relative flex gap-5 max-sm:gap-4 items-center h-[88px] pl-[11px] pr-[80px] max-xl:pr-[11px] xl:bg-white border-b-[1px] border-solid border-[#EDEDED]">
-      <div className="flex items-center xl:hidden gap-[6px]">
-        {/* <button
-          type="button"
-          className="relative"
-          onClick={() => setSidebarActive(true)}
-        >
-          <HiBars3 className="text-primary-main text-[24px]" />
-          <span className="block absolute -top-0.5 -right-0.5 border-2 border-white h-3 w-3 rounded-full bg-primary-main"></span>
-        </button> */}
-
-        {/* <Image src={'/#'} alt="logo" width={73} height={16} priority /> */}
-      </div>
-
       <div className="relative ml-auto">
         <LuBell className="text-[24px] text-gray-500" />
         <span className="block absolute top-0 right-0 border-[1px] border-white h-2 w-2 rounded-full bg-red-500"></span>
--- src/app/portal/courses/register/page.tsx
+++ src/app/portal/courses/register/page.tsx
@@ -1,5 +1,5 @@
-"use client";
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
+'use client'
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
 import {
   Select,
   SelectContent,
@@ -8,59 +8,89 @@ import {
   SelectLabel,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select";
-import { CourseList, TCourseObj } from "./components/CourseList";
-import { Label } from "@/components/ui/label";
-import { useEffect, useState } from "react";
-import { Button } from "@/components/ui/button";
-import { getSession, useSession } from "next-auth/react";
-import { redirect, useRouter } from "next/navigation";
+} from '@/components/ui/select'
+import { CourseList, TCourseObj } from './components/CourseList'
+import { Label } from '@/components/ui/label'
+import { useEffect, useState } from 'react'
+import { Button } from '@/components/ui/button'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog'
+import { usePaystackPayment } from 'react-paystack'
+import { v4 as uuid } from 'uuid'
 
 type TSelectSession = {
-  label: string;
-  value: string;
-};
+  label: string
+  value: string
+}
 
 type TSelectionProps = {
-  sessions: TSelectSession[];
-};
+  sessions: TSelectSession[]
+}
 
 const sessions = [
   {
-    label: "2023/2024",
-    value: "2023/2024",
+    label: '2023/2024',
+    value: '2023/2024',
   },
   {
-    label: "2022/2023",
-    value: "2022/2023",
+    label: '2022/2023',
+    value: '2022/2023',
   },
-];
+]
 
 const levels = [
-  { value: "nd-one", label: "ND I" },
-  { value: "nd-two", label: "ND II" },
-  { value: "hnd-one", label: "HND I" },
-  { value: "hnd-two", label: "HND II" },
-];
+  { value: 'nd-one', label: 'ND I' },
+  { value: 'nd-two', label: 'ND II' },
+  { value: 'hnd-one', label: 'HND I' },
+  { value: 'hnd-two', label: 'HND II' },
+]
 
 export default function RegisterCourses() {
-  const [selectedCourses, setSelectedCourses] = useState<TCourseObj[]>([]);
-  const [showCourseTable, setShowCourseTable] = useState(false);
+  const [selectedCourses, setSelectedCourses] = useState<TCourseObj[]>([])
+  const [showCourseTable, setShowCourseTable] = useState(false)
 
   const addToSelectedCourses = (course: TCourseObj) => {
-    setSelectedCourses([...selectedCourses, course]);
-  };
+    setSelectedCourses([...selectedCourses, course])
+  }
 
   const removeFromSelectedCourses = (id: number) => {
     setSelectedCourses((prevCourses) =>
       prevCourses.filter((course) => course.id !== id)
-    );
-  };
+    )
+  }
 
   const handleFetchCourse = () => {
     //TODO: while Check if the user has selected a course already in this collection and disable that course hence it's selected by that student already
-    setShowCourseTable(true);
-  };
+    setShowCourseTable(true)
+  }
+
+  const config = {
+    reference: uuid(),
+    email: 'student1@example.com', // Hardcoded email
+    amount: 50000, // Amount in Kobo, e.g., 50000 Kobo = N500
+    publicKey: process.env.NEXT_PUBLIC_PAYSTACK_PUBLIC_KEY!,
+  }
+
+  const onSuccess = (reference: string) => {
+    // Handle successful payment here
+    console.log('Successfully completed payment, reference ID: ', reference)
+  }
+
+  const onClose = () => {
+    // Handle Paystack dialog closure here
+    console.log('Paystack dialog closed')
+  }
+
+  const initializePayment = usePaystackPayment(config)
 
   return (
     <>
@@ -120,7 +150,10 @@ export default function RegisterCourses() {
                 </Select>
               </div>
               <div className="mt-10">
-                <Button className="w-full" onClick={handleFetchCourse}>
+                <Button
+                  className="w-full hover:bg-secondary hover:text-black"
+                  onClick={handleFetchCourse}
+                >
                   Process
                 </Button>
               </div>
@@ -144,9 +177,32 @@ export default function RegisterCourses() {
                   />
                   <hr />
                   <div className="flex justify-end mt-10">
-                    <Button className="" disabled={true}>
-                      Save Selected Course(s)
-                    </Button>
+                    <AlertDialog>
+                      <AlertDialogTrigger className="bg-black rounded-lg text-[#ffffff] w-full py-3 px-3 hover:bg-secondary hover:text-black">
+                        Continue
+                      </AlertDialogTrigger>
+                      <AlertDialogContent>
+                        <AlertDialogHeader>
+                          <AlertDialogTitle>
+                            Are you absolutely sure?
+                          </AlertDialogTitle>
+                          <AlertDialogDescription>
+                            You would need to pay ₦500.00 to register the
+                            course(s). Do you want to proceed?
+                          </AlertDialogDescription>
+                        </AlertDialogHeader>
+                        <AlertDialogFooter>
+                          <AlertDialogCancel>Cancel</AlertDialogCancel>
+                          <AlertDialogAction
+                            onClick={() => {
+                              initializePayment({ onSuccess, onClose })
+                            }}
+                          >
+                            Proceed
+                          </AlertDialogAction>
+                        </AlertDialogFooter>
+                      </AlertDialogContent>
+                    </AlertDialog>
                   </div>
                 </CardContent>
               </Card>
@@ -154,55 +210,25 @@ export default function RegisterCourses() {
           </div>
         </div>
       </div>
-      {/* <div className="flex gap-1 py-2 col-auto">
-            <Label htmlFor="level" className=" font-semibold">
-              Level:
-            </Label>
-
-          </div>
-          <div className="flex gap-1 py-2 col-auto">
-            <Label htmlFor="session" className=" font-semibold">
-              Sessions
-            </Label>
-            <SelectSession sessions={sessions} />
-          </div> */}
-      {/* <div className="grid grid-cols-4 gap-x-3 col-auto">
-            <div className="col-span-3">
-              <Card>
-                <CardHeader>
-                  <h4 className="mb-2 font-semibold">Available Courses</h4>
-                </CardHeader>
-                <CardContent className="px-2 pt-2">
-                  <CourseList
-                    addToSelectedCourses={addToSelectedCourses}
-                    removeFromSelectedCourses={removeFromSelectedCourses}
-                  />
-                </CardContent>
-              </Card>
-            </div>
-            <div className="col-span-1">
-              <SelectedCourseCard courses={selectedCourses} maxCredit={30} />
-            </div>
-          </div> */}
     </>
-  );
+  )
 }
 
-const SelectSession = ({ sessions }: TSelectionProps) => {
-  return (
-    <Select>
-      <SelectTrigger className="w-[180px]">
-        <SelectValue placeholder="Select a session" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          {sessions.map((item) => (
-            <SelectItem key={item.value} value={item.value}>
-              {item.label}
-            </SelectItem>
-          ))}
-        </SelectGroup>
-      </SelectContent>
-    </Select>
-  );
-};
+// const SelectSession = ({ sessions }: TSelectionProps) => {
+//   return (
+//     <Select>
+//       <SelectTrigger className="w-[180px]">
+//         <SelectValue placeholder="Select a session" />
+//       </SelectTrigger>
+//       <SelectContent>
+//         <SelectGroup>
+//           {sessions.map((item) => (
+//             <SelectItem key={item.value} value={item.value}>
+//               {item.label}
+//             </SelectItem>
+//           ))}
+//         </SelectGroup>
+//       </SelectContent>
+//     </Select>
+//   )
+// }
